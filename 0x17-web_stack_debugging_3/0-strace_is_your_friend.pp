71cbc31fd83b268b62c9bf27543929efb266270f
Joshua Ajayi <kenduajayi@gmail.com>
2023-11-09 01:43:34 +0000

    project task one

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fix bad `phpp` extensions to `php` in WordPress file `wp-settings.php`.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
